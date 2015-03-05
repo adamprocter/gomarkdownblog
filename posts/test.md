b98cb41462daeb90e78f139c3f5e88d3422a0f42
Adam Procter <adamprocter@mac.com>
2015-03-05 14:36:39 +0000

    version 1
    uploading basic files and set up for the go lang markdown blog

---
+++ posts/test.md
@@ -0,0 +1,8 @@
+Hello Go lang!
+12th Jan 2015
+This is a basic start to my own hosted go lang  markdown static blog/ web generator.
+### Here I am...
+
+This entry is a no whistles Hello World entry as I have the basic functionality of a go server and static file templating engine up and running and it should render from 
+#### Markdown files which
+*is* **lovely**!
---
+++ web_server.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	//"fmt"
+	"database/sql"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"path/filepath"
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/russross/blackfriday"
+	
+)
+
+
+type Post struct {
+	Title    string
+	Date     string
+	Summary  string
+	Body     template.HTML
+	File     string
+	Comments []Comment
+}
+
+type Comment struct {
+	Name, Comment string
+}
+
+var db *sql.DB
+
+func init() {
+	// you do not have to open the db connection on every request
+	// it can be done once at the start of the app
+	var err error
+	db, err = sql.Open("mysql", "username:password(localhost:3306)/database")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Open doesn't open a connection. Validate DSN data:
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func handlerequest(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+
+		uniquepost := r.FormValue("uniquepost")
+		namein := r.FormValue("name")
+		commentin := r.FormValue("comment")
+	
+		_, err := db.Exec(
+			"INSERT INTO comments (uniquepost, name, comment) VALUES (?, ?, ?)",
+			uniquepost,
+			namein,
+			commentin,
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		//when done inserting comment redirect back to this page
+		http.Redirect(w, r, r.URL.Path, 301)
+		return
+	}
+
+	if r.URL.Path[1:] == "" {
+		posts := getPosts()
+		t := template.New("index.html")
+		t, _ = t.ParseFiles("index.html")
+		t.Execute(w, posts)
+		return
+	}
+	uniquepost := r.URL.Path[1:]
+	rows, err := db.Query("select id, name, comment from comments where uniquepost = ?", uniquepost)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	//declear an array to keep all comments
+	var comments []Comment
+
+	for rows.Next() {
+		var id int
+		var name, comment string
+		err := rows.Scan(&id, &name, &comment)
+		if err != nil {
+			log.Fatal(err)
+		}
+		//append the comment into the array when done
+		comments = append(comments, Comment{name, comment})
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f := "posts/" + r.URL.Path[1:] + ".md"
+	fileread, _ := ioutil.ReadFile(f)
+	lines := strings.Split(string(fileread), "\n")
+	title := string(lines[0])
+	date := string(lines[1])
+	summary := string(lines[2])
+	body := strings.Join(lines[3:len(lines)], "\n")
+	htmlBody := template.HTML(blackfriday.MarkdownCommon([]byte(body)))
+	post := Post{title, date, summary, htmlBody, r.URL.Path[1:], comments}
+	t := template.New("post.html")
+	t, _ = t.ParseFiles("post.html")
+	t.Execute(w, post)
+
+}
+
+func getPosts() []Post {
+	a := []Post{}
+	files, _ := filepath.Glob("posts/*")
+	for _, f := range files {
+		file := strings.Replace(f, "posts/", "", -1)
+		file = strings.Replace(file, ".md", "", -1)
+		fileread, _ := ioutil.ReadFile(f)
+		lines := strings.Split(string(fileread), "\n")
+		title := string(lines[0])
+		date := string(lines[1])
+		summary := string(lines[2])
+		body := strings.Join(lines[3:len(lines)], "\n")
+		htmlBody := template.HTML(blackfriday.MarkdownCommon([]byte(body)))
+
+		a = append(a, Post{title, date, summary, htmlBody, file, nil})
+	}
+	return a
+}
+
+func main() {
+	http.HandleFunc("/", handlerequest)
+	http.ListenAndServe(":8000", nil)
+}
